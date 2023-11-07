ef4d6f0f872f45fabfc016024ecf5c51795a4909
HTLinh1908 <147149610+HTLinh1908@users.noreply.github.com>
2023-11-07 21:00:11 +0700

    Update ListofRoom.py

--- ListofRoom.py
+++ ListofRoom.py
@@ -1,1 +1,10 @@
+import numpy as np
 List = ['CR1','CR2']
+class Classroom():
+    def __init__(self, room, type):
+        self.room = room
+        self.status = np.array([np.zeros(5)]*6)
+        self.type = type
+lmouse = Classroom('CR1')
+
+print(lmouse.status)
