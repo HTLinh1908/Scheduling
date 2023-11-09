653789c322a17cd67c41f347f0be37aa4a961f56
HTLinh1908 <hoangtuanlinh1908@gmail.com>
2023-11-09 17:14:00 +0700

    Add 2 function :check the room status                 change the room status

--- Classroom.py
+++ Classroom.py
@@ -5,6 +5,23 @@ class Classroom():
         self.room = room
         self.status = np.array([np.zeros(5)]*6)
         self.type = type
-lmouse = Classroom('Classroom 1')
+    
+    def check_if_free(self, day, time):
+        return True if self.status[day,time]==0 else False
+    
+    def book_a_time(self, day, time):
+        if self.check_if_free(day,time)==True:
+            self.status[day,time]=1
+            return True
+        return False
 
-print(lmouse.status)
+test = Classroom("Classroom1",1)
+print(test.status)
+
+print(test.check_if_free(0,0))
+
+test.book_a_time(0,0)
+print(test.status)
+
+print(test.check_if_free(0,0))
+
