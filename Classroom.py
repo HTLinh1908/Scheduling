ea182475b948cfd28e0077952f5d5da157e3bd63
HTLinh1908 <hoangtuanlinh1908@gmail.com>
2023-12-18 22:52:59 +0700

    y

--- Classroom.py
+++ Classroom.py
@@ -52,7 +52,10 @@ class Department:
     def get_course(self): return self.course
 
 class Instructor:
-    def __init__(self, id, name): self.name = name
+    def __init__(self, id, name): 
+        self.id= id
+        self.name = name
+    def get_id(self): return self.id
     def get_name(self): return self.name
 
 class Class:
