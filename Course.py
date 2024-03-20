34f1cfab3af9b3c13bd493599679fff9e92bc755
HTLinh1908 <hoangtuanlinh1908@gmail.com>
2024-03-20 08:23:57 +0700

    Update stuff

---
+++ Course.py
@@ -0,0 +1,112 @@
+import numpy as np
+class Course:
+    room = ""
+    slot = (-1, -1)
+
+    def __init__(self, name, time_slots, capacity):
+        self.name = name
+        # the course code, E.g. CS302, MATH101
+
+        self.time_slots = time_slots
+        # 3 possible time slots, each time slot is represented by
+        # a list of a tuple class time information: (day_i, starting time_i, duration_i)
+        # Example for one preferred time slot: [(0, 8, 1.5), (2, 8, 1.5)]
+        # means the course first preferred time slot is Monday and Wednesday from 8:00 to 9:30
+        # The same applies for the second and third preferred time slots.
+
+        self.capacity = capacity
+
+    def get_timeslots(self, index):
+        return self.time_slots[index]
+
+    def get_capacity(self):
+        return self.capacity
+
+    def get_name(self):
+        return self.name
+
+
+class Classroom:
+    def __init__(self, name, capacity):
+        self.name = name
+        self.capacity = capacity
+        self.availability = [[0 for _ in range(5)] for _ in range(5)]
+
+    def get_capacity(self):
+        return self.capacity
+
+
+def timeslots_matrix(timeslots_list):
+    matrix = np.zeros((5, 6))
+    for i in timeslots_list:
+        if i[0][2] == 1.5:
+            matrix[i[0][0], i[0][1]] += 1
+            matrix[i[0][0]+2, i[0][1]] += 1
+        if i[0][2] == 3:
+            matrix[i[0][0], i[0][1]] += 1
+            matrix[i[0][0], i[0][1]+1] += 1
+    return matrix
+
+
+def check_overlapped(list_of_course, chromosome):
+    timeslots_list = filter_timeslots(list_of_course, chromosome)
+    penalty = 0
+
+    if np.any(timeslots_matrix(timeslots_list) > 1):
+        penalty += 1000
+
+    return penalty
+
+
+def filter_timeslots(list_of_course, chromosome):
+    timeslots_list = []
+    chromosome_list = [int(i) for i in list(chromosome)]
+    index = 0
+    for element in list_of_course:
+        timeslots_list.append(element.get_timeslots(chromosome_list[index]))
+        index += 1
+    return timeslots_list
+
+
+def check_availability(list_of_course, chromosome, list_of_classroom):
+    classroom_capacity = []
+    course_capacity_matrix = []
+    for element in list_of_classroom:
+        classroom_capacity.append(element.get_capacity())
+    timeslots_list = filter_timeslots(list_of_course, chromosome)
+
+
+def capacity_matrix(list_of_course, chromosome, list_of_classroom):
+    global classrooms
+    matrix = np.zeros((5, 6, len(classrooms)))
+    classroom_capacity = []
+    for room in list_of_classroom:
+        classroom_capacity.append(room.get_capacity())
+
+
+courses = [
+    Course("101", [[(0, 0, 1.5), (2, 0, 1.5)], [(0, 0, 1.5), (2, 0, 1.5)], [(1, 2, 1.5), (3, 2, 1.5)]], 20),
+    Course("102", [[(0, 0, 3)], [(0, 0, 3)], [(4, 1, 3)]], 20),
+    Course("103", [[(0, 0, 1.5), (3, 2, 1.5)], [(1, 3, 1.5), (3, 3, 1.5)], [(0, 3, 1.5), (2, 3, 1.5)]], 40),
+    Course("104", [[(0, 0, 1.5), (3, 2, 1.5)], [(0, 3, 1.5), (2, 3, 1.5)], [(0, 3, 1.5), (2, 3, 1.5)]], 30),
+    Course("105", [[(0, 0, 1.5), (2, 2, 1.5)], [(1, 3, 1.5), (3, 3, 1.5)], [(1, 1, 1.5), (3, 1, 1.5)]], 30),
+]
+classrooms = [
+    Classroom("CR501", 70),
+    Classroom("CR502", 65),
+    Classroom("CR1", 55),
+    Classroom("CR2", 25),
+    Classroom("CR4", 45),
+    Classroom("CR3", 30),
+    Classroom("CR5", 15),
+    Classroom("CR6", 35),
+    Classroom("CR7", 35),
+    Classroom("CR8", 45),
+]
+classrooms = sorted(classrooms, key = lambda x : x.capacity)
+
+print(check_overlapped(courses, '00000'))
+
+
+
+#print(check_overlapped(courses, '00000'))
--- Test.py
+++ Test.py
@@ -1,140 +1,83 @@
 import numpy as np
 
 
-class Classroom():
+
+class Classroom:
     def __init__(self, name, capacity):
         self.name = name
         self.capacity = capacity
-        self.status = [[0 for _ in range(5)] for _ in range(5)] 
-        #print(self.status)
+        self.status = np.zeros((5,6), dtype=int)
+        # print(self.status)
+    def __str__(self):
+        return 'Classroom name: ' + str(self.name) + ' capacity: ' + str(self.capacity) + ' status: ' + str(self.capacity)
 
-class Course():
+
+class Course:
     room = ""
     slot = (-1, -1)
-    def __init__(self, name, time_slots, capacity):
-        self.name = name 
-        #the course code, E.g. CS302, MATH101
 
-        self.time_slots = time_slots 
+    def __init__(self, name, time_slots, capacity):
+        self.name = name
+        # the course code, E.g. CS302, MATH101
+        self.time_slots = time_slots
         # 3 possible time slots, each is a tuple (day, starting time), 
         # assumed that each course is 1h30m long
 
         self.capacity = capacity
 
-class Schedule():
-    
-    table = [[[] for _ in range(5)] for _ in range(5)] 
-    distribution = [[0 for j in range(5)] for i in range(5)]
+    def __str__(self):
+        return 'Course name: ' + str(self.name) + ' time_slots: ' + str(self.time_slots) + ' capacity: ' + str(self.capacity)
+    def get_time_slots(self):
+        return self.time_slots
+def update_matrix(arr_list):
+    # Initialize the matrix with zeros
+    matrix = np.zeros((5, 6))
+
+    # Update the matrix based on the coordinates in arr_list
+    for x, y in arr_list:
+        if 0 <= x < 5 and 0 <= y < 6:
+            matrix[x, y] += 1
+
+        return matrix
 
 
-    def add_course(self, course):
-        for day, time in course.time_slots:
-            self.table[day][time].append(course)
-            self.distribution[day][time] += 1
-            
-    def remove_course(self, course):
-        for day, time in course.time_slots:
-            for tmp in self.table[day][time]:
-                if (tmp.name == course.name):
-                    self.table[day][time].remove(tmp)
-                    self.distribution[day][time] -= 1
-
-    def find_course_min_indices(self):
-        min_value = 9999
-        min_i, min_j = (-1, -1)
-
-        for i in range(5):
-            for j in range(5):
-                if (self.distribution[i][j] > 0 and self.distribution[i][j] < min_value):
-                    min_value = self.distribution[i][j]
-                    min_i, min_j = i, j
-
-        return min_i, min_j
-
-    
 def main():
     classrooms = [
-    #Classroom("CR501", 70),
-    Classroom("CR502", 65),
-    Classroom("CR1", 55),
-    Classroom("CR2", 25),
-    Classroom("CR4", 45),
-    #Classroom("CR3", 30),
-    Classroom("CR5", 15),
-    Classroom("CR6", 35),
-    Classroom("CR7", 35),
-    Classroom("CR8", 45),
+        # Classroom("CR501", 70),
+        Classroom("CR502", 65),
+        Classroom("CR1", 55),
+        Classroom("CR2", 25),
+        Classroom("CR4", 45),
+        # Classroom("CR3", 30),
+        Classroom("CR5", 15),
+        Classroom("CR6", 35),
+        Classroom("CR7", 35),
+        Classroom("CR8", 45),
     ]
-    classrooms = sorted(classrooms, key = lambda x : x.capacity) 
-    #increasing capacity order
+    classrooms = sorted(classrooms, key=lambda x: x.capacity)
+    # increasing capacity order
 
     courses = [
-        Course("101", [(1, 1), (3, 2)], 50), 
-        Course("102", [(1, 4), (2, 0)], 30), 
-        Course("103", [(2, 4), (1, 0)], 40), 
-        Course("104", [(3, 0), (2, 2)], 50), 
-        Course("105", [(3, 2), (2, 2)], 40), 
-        Course("106", [(0, 4), (4, 2)], 50), 
-        Course("107", [(0, 3), (3, 1)], 10), 
-        Course("108", [(0, 0), (4, 4)], 30), 
-        Course("109", [(4, 2), (3, 4)], 30), 
-        Course("110", [(3, 4), (2, 3)], 30), 
-        Course("111", [(3, 4), (2, 3)], 10), 
-        Course("112", [(2, 1), (4, 1)], 30), 
-        Course("113", [(2, 0), (4, 3)], 30), 
-        Course("114", [(2, 3), (1, 4)], 40), 
-        Course("115", [(1, 0), (3, 4)], 10), 
-        Course("116", [(3, 2), (1, 1)], 10), 
-        Course("117", [(4, 3), (0, 4)], 30), 
-        Course("118", [(4, 1), (1, 4)], 10), 
-        Course("119", [(1, 0), (1, 1)], 10), 
-        Course("120", [(3, 0), (1, 0)], 40), 
+        Course("101", [(1, 1), (3, 2)], 50),
+        Course("102", [(1, 4), (2, 0)], 30),
+        Course("103", [(2, 4), (1, 0)], 40),
+        Course("104", [(3, 0), (2, 2)], 50),
+        Course("105", [(3, 2), (2, 2)], 40),
+        Course("106", [(0, 4), (4, 2)], 50),
+        Course("107", [(0, 3), (3, 1)], 10),
+        Course("108", [(0, 0), (4, 4)], 30),
+        Course("109", [(4, 2), (3, 4)], 30),
+        Course("110", [(3, 4), (2, 3)], 30),
+        Course("111", [(3, 4), (2, 3)], 10),
+        Course("112", [(2, 1), (4, 1)], 30),
+        Course("113", [(2, 0), (4, 3)], 30),
+        Course("114", [(2, 3), (1, 4)], 40),
+        Course("115", [(1, 0), (3, 4)], 10),
+        Course("116", [(3, 2), (1, 1)], 10),
+        Course("117", [(4, 3), (0, 4)], 30),
+        Course("118", [(4, 1), (1, 4)], 10),
+        Course("119", [(1, 0), (1, 1)], 10),
+        Course("120", [(3, 0), (1, 0)], 40),
     ]
-    schedule = Schedule()
-
-    for course in courses:
-        schedule.add_course(course)
-
-
-    for i in range(5):
-        for j in range(5):
-            schedule.table[i][j] = sorted(schedule.table[i][j], key = lambda x : x.capacity)
-
-    final_result = [] #list of assigned courses
-
-    ok = False
-    while (not ok):
-        min_i, min_j = schedule.find_course_min_indices()
-        if (min_i == -1 and min_j == -1):
-            ok = True
-            break
-
-        course = schedule.table[min_i][min_j][0]
-
-        for classroom in classrooms:
-            if (classroom.status[min_i][min_j] == 0 and classroom.capacity >= course.capacity):
-
-                #assign course and classroom
-                classroom.status[min_i][min_j] = course
-                course.room = classroom
-                course.slot = (min_i, min_j)
-                final_result.append(course)
-            
-                #mark course as processed and remove from distribution
-                schedule.remove_course(course)
-
-                break
-
-    for course in final_result:
-        print(f"{course.name}: {course.room.name}, {course.slot}")
-    
-    classrooms = sorted(classrooms, key = lambda x : x.name)
-    
-    for classroom in classrooms:
-        print(f"{classroom.name}:")
-        for i in range(5):
-            for j in range(5):
-                if (classroom.status[i][j] != 0):
-                    print(f"Day {i}, Time slot {j}: Course {classroom.status[i][j].name}")
 
 main()
