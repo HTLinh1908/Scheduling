6ea312a05838f10223e731b843f377631b33b223
JarvisHunter <minhtuantn2406@gmail.com>
2024-05-26 21:42:41 +0700

    now able to process ENG and DST courses

--- heuristic_main.py
+++ heuristic_main.py
@@ -6,39 +6,57 @@ timeslot_priority_list = [0, 1, 2, 3, 4, 5]
 
 res = [[["" for _ in range(len(classrooms))] for _ in range(6)] for _ in range(5)]
 
-courses = sorted(courses, key=lambda x: (x.level, x.type, -x.capacity))
+#engineering and DST courses
+technical_courses = [course for course in courses if course.major == "ENG" or course.name == "CORE105"]
+technical_courses = sorted(technical_courses, key=lambda x: (x.level, x.type, -x.capacity))
+technical_classrooms = [classroom for classroom in classrooms
+                        if classroom.name == "Makerspace"]
+technical_classrooms = sorted(technical_classrooms, key=lambda x: x.capacity)
 
-for course in courses:
-    for j in range(len(classrooms)):
-        if course.capacity <= classrooms[j].capacity:
-            if course.type == 0:  # type 0: 1.5h
-                for k in timeslot_priority_list:
-                    if course.chosen_slot != (-1, -1, -1):
-                        continue
-                    if res[0][k][j] == "" and res[2][k][j] == "":
-                        res[0][k][j] = course.name
-                        res[2][k][j] = course.name
-                        course.chosen_slot = [(0, k, j), (2, k, j)]
-                        break
-                    if res[1][k][j] == "" and res[3][k][j] == "":
-                        res[1][k][j] = course.name
-                        res[3][k][j] = course.name
-                        course.chosen_slot = [(1, k, j), (3, k, j)]
-                        break
-            else:  # type 1: 3h
-                for k in range(len(timeslot_priority_list) - 1):  # latest 3h course can just be 15:00
-                    for day in range(4, -1, -1):
+#other courses
+courses = [course for course in courses if course.major != "ENG" and course.name != "CORE105"]
+courses = sorted(courses, key=lambda x: (x.level, x.type, -x.capacity))
+non_technical_classrooms = [classroom for classroom in classrooms
+                            if classroom.name != "Makerspace"]
+non_technical_classrooms = sorted(non_technical_classrooms, key=lambda x: x.capacity)
+
+
+def assign_course(courses, available_classrooms):
+    for course in courses:
+        for j in range(len(available_classrooms)):
+            if course.capacity <= available_classrooms[j].capacity:
+                if course.type == 0:  # type 0: 1.5h
+                    for k in timeslot_priority_list:
                         if course.chosen_slot != (-1, -1, -1):
                             continue
-                        if res[day][k][j] == "" and res[day][k+1][j] == "":
-                            res[day][k][j] = course.name
-                            res[day][k+1][j] = course.name
-                            course.chosen_slot = (day, k, j)
+                        if res[0][k][available_classrooms[j].id] == "" and res[2][k][available_classrooms[j].id] == "":
+                            res[0][k][available_classrooms[j].id] = course.name
+                            res[2][k][available_classrooms[j].id] = course.name
+                            course.chosen_slot = [(0, k, available_classrooms[j].id), (2, k, available_classrooms[j].id)]
                             break
+                        if res[1][k][available_classrooms[j].id] == "" and res[3][k][j] == "":
+                            res[1][k][available_classrooms[j].id] = course.name
+                            res[3][k][available_classrooms[j].id] = course.name
+                            course.chosen_slot = [(1, k, available_classrooms[j].id), (3, k, available_classrooms[j].id)]
+                            break
+                else:  # type 1: 3h
+                    for k in range(len(timeslot_priority_list) - 1):  # latest 3h course can just be 15:00
+                        for day in range(4, -1, -1):
+                            if course.chosen_slot != (-1, -1, -1):
+                                continue
+                            if res[day][k][available_classrooms[j].id] == "" and res[day][k+1][available_classrooms[j].id] == "":
+                                res[day][k][available_classrooms[j].id] = course.name
+                                res[day][k+1][available_classrooms[j].id] = course.name
+                                course.chosen_slot = (day, k, available_classrooms[j].id)
+                                break
 
+
+assign_course(technical_courses, technical_classrooms)
+assign_course(courses, non_technical_classrooms)
 """OUTPUT"""
 time_in_day = ["8AM", "9:45AM", "11:30AM", "1:15PM", "3PM", "4:45PM"]
 
+classrooms = sorted(classrooms, key=lambda x: x.id)
 with open("output.txt", "w") as f:
     for day in range(5):
         f.write(" ")
